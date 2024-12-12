5a5d70de82ee184b51a9be296e86f66151730daf
Nicholas Miklaucic <nicholas.miklaucic@gmail.com>
2024-12-11 22:43:04 -0500

    New keyboard model, fixed evaluation

--- src/critic/__init__.py
+++ src/critic/__init__.py
@@ -25,12 +25,13 @@ def load_combo_corrector():
     return corrector
 
 
-def load_llama_corrector(n_threads=2, **kwargs):
+def load_llama_corrector(n_threads=2, new_kbd=True, kbd_weight=1, **kwargs):
     from critic.llama import Llama, LlamaAdapter
 
     model = KbdModel()
 
-    model.load_weights("models/kbd_model.weights.h5")
+    path = "kbd_model_new" if new_kbd else "kbd_model"
+    model.load_weights(f"models/{path}.weights.h5")
     model.kbd_layout_distr = cache(model.kbd_layout_distr)
     llm = Llama(
         "models/llama-3.2-1b-q4_k_m.gguf",
@@ -43,7 +44,7 @@ def load_llama_corrector(n_threads=2, **kwargs):
         **kwargs,
     )
     kbd = KbdCorrector(model)
-    corrector = LlamaAdapter(llm, base=kbd, cache_state=True)
+    corrector = LlamaAdapter(llm, base=kbd, kbd_weight=kbd_weight, cache_state=True)
     return corrector
 
 
